93149463e7a5a764962572bbdf718c278f56ec8f
ItsMuri <88394932+ItzMuri@users.noreply.github.com>
2025-03-08 14:44:45 +0100

    Update functions.lua

--- config/functions.lua
+++ config/functions.lua
@@ -6,16 +6,17 @@ return {
             return LocalPlayer.state.isSeatbeltOn or false -- Adjust based on your framework
         end
     end
-
-    getVehicleFuel = function(currentVehicle)
+   getVehicleFuel = function(currentVehicle)
 		if GetResourceState('ps-fuel') == 'started' then
 			return exports['ps-fuel']:GetFuel(currentVehicle)
 		elseif GetResourceState('cdn-fuel') == 'started' then
 			return exports['cdn-fuel']:GetFuel(currentVehicle)
 		elseif GetResourceState('LegacyFuel') == 'started' then
 			return exports['LegacyFuel']:GetFuel(currentVehicle)
+		elseif GetResourceState('ox_fuel') == 'started' then
+			return Entity(currentVehicle).state.fuel
 		else
-			return Entity(currentVehicle).state.fuel or 100.0
+			return GetVehicleFuelLevel(currentVehicle)
 		end
 	end	
     getNosLevel = function(currentVehicle) -- Replace this with your own logic to grab the nos level of the vehicle.
